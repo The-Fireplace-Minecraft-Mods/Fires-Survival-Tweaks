ef1344f58305807a808c1f87976c7c69d317db33
The_Fireplace <the.f1repl4ce@gmail.com>
2016-02-06 12:43:11 -0600

    Updated to 1.8.9

--- src/main/java/the_fireplace/realstonetools/RealStoneTools.java
+++ src/main/java/the_fireplace/realstonetools/RealStoneTools.java
@@ -1,7 +1,5 @@
 package the_fireplace.realstonetools;
 
-import java.util.ArrayList;
-
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -10,10 +8,12 @@ import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 
-@Mod(modid="realstonetools", name="Real Stone Tools", version="2.0.1.0")
+import java.util.ArrayList;
+
+@Mod(modid="realstonetools", name="Real Stone Tools")
 public class RealStoneTools {
 	@EventHandler
 	public void init(FMLInitializationEvent event){
@@ -28,17 +28,16 @@ public class RealStoneTools {
 		removeRecipe(shovel);
 		removeRecipe(pickaxe);
 		ItemStack stone = new ItemStack(Blocks.stone);
-		OreDictionary.registerOre("stick", Items.stick);
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(axe, "rr", "rs", " s", 'r', stone, 's', "stick"));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(axe, "rr", "sr", "s ", 'r', stone, 's', "stick"));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(hoe, "rr", " s", " s", 'r', stone, 's', "stick"));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(hoe, "rr", "s ", "s ", 'r', stone, 's', "stick"));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(sword, "r", "r", "s", 'r', stone, 's', "stick"));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(shovel, "r", "s", "s", 'r', stone, 's', "stick"));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(pickaxe, "rrr", " s ", " s ", 'r', stone, 's', "stick"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(axe, "rr", "rs", " s", 'r', stone, 's', "stickWood"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(axe, "rr", "sr", "s ", 'r', stone, 's', "stickWood"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(hoe, "rr", " s", " s", 'r', stone, 's', "stickWood"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(hoe, "rr", "s ", "s ", 'r', stone, 's', "stickWood"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(sword, "r", "r", "s", 'r', stone, 's', "stickWood"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(shovel, "r", "s", "s", 'r', stone, 's', "stickWood"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(pickaxe, "rrr", " s ", " s ", 'r', stone, 's', "stickWood"));
 	}
 	private void removeRecipe(ItemStack resultItem){
-		ItemStack recipeResult = null;
+		ItemStack recipeResult;
 		ArrayList recipes = (ArrayList) CraftingManager.getInstance().getRecipeList();
 		for(int scan = 0;scan < recipes.size();scan++){
 			IRecipe tmpRecipe = (IRecipe) recipes.get(scan);
