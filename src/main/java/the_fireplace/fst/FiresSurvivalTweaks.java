43b6ae4d7bc825f5c77eeb515217fde57184181a
The_Fireplace <the.f1repl4ce@gmail.com>
2020-10-03 11:15:57 -0500

    Implement Blaze Powder Nether Crop Growth

--- src/main/java/the_fireplace/fst/FiresSurvivalTweaks.java
+++ src/main/java/the_fireplace/fst/FiresSurvivalTweaks.java
@@ -3,7 +3,14 @@ package the_fireplace.fst;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import me.sargunvohra.mcmods.autoconfig1u.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.NetherWartBlock;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import the_fireplace.fst.datagen.AdditiveDataGenerator;
@@ -22,6 +29,30 @@ public class FiresSurvivalTweaks implements ModInitializer {
 	public void onInitialize() {
 		AutoConfig.register(ModConfig.class, JanksonConfigSerializer::new);
 		config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+
+		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+			if(!config.enableBlazePowderNetherCropGrowth)
+				return ActionResult.PASS;
+			BlockState state = world.getBlockState(hitResult.getBlockPos());
+			ItemStack stack = player.getStackInHand(hand);
+			if(!player.world.isClient()
+				&& state.isOf(Blocks.NETHER_WART)
+				&& stack.getItem().equals(Items.BLAZE_POWDER)) {
+				int age = state.get(NetherWartBlock.AGE);
+				if(age < 3) {
+					state = state.getBlock().getDefaultState().with(NetherWartBlock.AGE, Math.min(3, world.random.nextInt(3 - age) + age + 1));
+					world.setBlockState(hitResult.getBlockPos(), state);
+					if (!player.isCreative()) {
+						if(stack.getCount() > 1)
+							stack.decrement(1);
+						else
+							player.setStackInHand(hand, ItemStack.EMPTY);
+					}
+					return ActionResult.SUCCESS;
+				}
+			}
+			return ActionResult.PASS;
+		});
 
 		//noinspection ConstantConditions
 		if(false) {
