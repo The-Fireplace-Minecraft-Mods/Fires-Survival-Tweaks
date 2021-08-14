593258c7d2e448c13fc87788c0a907d093dddb4c
The_Fireplace <the.f1repl4ce@gmail.com>
2021-08-14 09:41:00 -0500

    Don't use nested recursive calls when causing cave-ins. Close #12

--- src/main/java/dev/the_fireplace/fst/logic/CaveinLogic.java
+++ src/main/java/dev/the_fireplace/fst/logic/CaveinLogic.java
@@ -14,15 +14,19 @@ public class CaveinLogic {
     public static final int MAX_TREMOR_RANGE = 32;
 
     public static void cavein(World world, BlockPos fieldCenter, int tremorRange, int tremorChanceCount) {
-        if (tremorChanceCount <= 0) {
-            return;
+        for (int remainingTremors = tremorChanceCount; remainingTremors > 0; remainingTremors--) {
+            tremor(world, fieldCenter, tremorRange, remainingTremors);
+            tremorRange = (int)Math.ceil(tremorRange*0.95);
         }
+    }
+
+    private static void tremor(World world, BlockPos fieldCenter, int tremorRange, int remainingIterations) {
         int xOff = (world.random.nextBoolean() ? 1 : -1) * world.random.nextInt(tremorRange) + 1;
-        int maxYOffset = Math.min(world.getHeight() - fieldCenter.getY() - 1, fieldCenter.getY() + 1) - world.getDimension().getMinimumY();
-        if (maxYOffset < 1) {
-            maxYOffset = 1;
+        int yRange = Math.min(world.getHeight() - fieldCenter.getY() - 1, fieldCenter.getY() + 1) - world.getDimension().getMinimumY();
+        if (yRange < 1) {
+            yRange = 1;
         }
-        int yOff = (world.random.nextInt(tremorRange) + 1) % maxYOffset;
+        int yOff = (world.random.nextInt(tremorRange) + 1) % yRange;
         int zOff = (world.random.nextBoolean() ? 1 : -1) * world.random.nextInt(tremorRange) + 1;
         BlockPos targetPos = fieldCenter.add(xOff, yOff, zOff);
         //TODO Stability checking based on how many rocks have supports?
@@ -37,12 +41,11 @@ public class CaveinLogic {
             BlockState state = world.getBlockState(pos);
             if (state.isIn(FSTBlockTags.FALLING_ROCKS) && FallingBlock.canFallThrough(world.getBlockState(pos.down()))) {
                 makeBlockFall(world, pos, state);
-                if (world.random.nextInt(tremorRange*tremorRange/((tremorChanceCount/3)+1)+1) == 0) {
-                    cavein(world, pos, (int) Math.ceil(tremorRange * 0.6), 1);
+                if (remainingIterations > 1 && world.random.nextInt(tremorRange * tremorRange /((remainingIterations /3)+1)+1) == 0) {
+                    tremor(world, pos, (int) Math.ceil(tremorRange * 0.6), 1);
                 }
             }
         }
-        cavein(world, fieldCenter, (int)Math.ceil(tremorRange*0.95), tremorChanceCount - 1);
     }
 
     public static void makeBlockFall(World world, BlockPos pos, @Nullable BlockState state) {
